6e8f3557db262b9b3e71325a4511b698cb8acd7d
innocentzy <nikitka.kat@mail.ru>
2026-03-12 12:08:08 +0300

    Add DBs for dev/prod

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/bookingservice"
+    DATABASE_URL: str = "sqlite+aiosqlite:///db.sqlite3"  # FOR DEV
+    # DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/bookingservice" # FOR PROD
     SECRET_KEY: str = "dev-secret-key-change-me"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
